3ada9626149f486ae879ce091e5bf4ea4fcdb09f
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-01-05 00:23:08 +0530

    Create Minimum_Number_of_Operations_to_make_Array_Empty.py

---
+++ Minimum_Number_of_Operations_to_make_Array_Empty.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        
+        mp = Counter(nums)
+
+        count = 0
+        for t in mp.values():
+            if t == 1:
+                return -1
+            count += t // 3
+            if t % 3:
+                count += 1
+
+        return count
